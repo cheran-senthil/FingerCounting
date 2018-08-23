7a1054518544ed18b934d9cc4d4710f3f83f6cfa
Cheran Senthil <cheran.v.senthil@gmail.com>
2018-08-23 19:24:01 +0530

    Add tree

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import numpy as np
 import cv2
+from sklearn import tree
+
+X = np.zeros((1, 307200))
+Y = [[0]]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
 
 cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
+while True:
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+    clf.predict([gray.ravel()])
+
 cap.release()
 cv2.destroyAllWindows()
